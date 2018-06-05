49c8f87bb60d3f7cde3b380c81e1ce126d5da03c
Bahtiyar <bahtiyar@Bahtiyars-MacBook-Pro.local>
2018-06-05 17:27:47 +0800

    many things

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-v3-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+const API_KEY = 'AIzaSyA4iQq75FMNycOWrfYEFhGumk6kqXP6Rcs';
 
-const App = () => {
-    return <div>Hiii</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        }
+        YTSearch(API_KEY, {q: 'node js'})
+            .then((result) => {
+                this.setState({videos: result.items});
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    render() {
+        return <div>
+            <SearchBar />
+            <VideoDetail video={this.state.videos[0]} />
+            <VideoList videos={this.state.videos} />
+        </div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
